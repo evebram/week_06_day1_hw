627154bcea5cad692a0640a3f84ba37c7cee052a
= <evebramley@me.com>
2019-08-05 17:13:37 +0100

    extension 1 done

--- src/app.js
+++ src/app.js
@@ -4,12 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      list: [
-        {chore: 'Buy shopping'},
-        {chore: 'Clean bathroom'},
-        {chore: 'Car MOT'}
+      items: [
+        {name: 'Buy shopping', priority: 'High'},
+        {name: 'Clean bathroom', priority: 'Low'},
+        {name: 'Car MOT', priority: 'High'}
       ],
       newChore: ''
+    },
+    methods: {
+      addNewChore: function (e) {
+        e.preventDefault();
+        this.items.push({
+          name: this.newChore,
+          priority: this.newPriority
+        });
+        this.newChore = '';
+      }
     }
-  })
-})
+  });
+});
